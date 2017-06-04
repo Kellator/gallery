cbccc6a0bdd262fd42b0984f72619af20384ecbc
Kellator <k.macleod21@gmail.com>
2017-06-04 18:04:42 -0400

    removed mongo info from config file

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
-                        (process.env.NODE_ENV === 'production' ?
-                        'mongodb://gallery_user:t@pD@nc3r@ds161931.mlab.com:61931/gallery' :
-                        'mongodb://localhost/gallery-dev');
+                        'mongodb://localhost/gallery-dev';
 exports.PORT = process.env.PORT || 8080;
