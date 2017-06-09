77bcdd792c1fd4ba1a79ddba90c445d5bc76be85
Kellator <k.macleod21@gmail.com>
2017-06-08 21:01:06 -0400

    added basic components to previous files

--- js/components/presentational/login_block.js
+++ js/components/presentational/login_block.js
@@ -1,9 +1,10 @@
-//landing page contains the log-in component and link to set up new user profile
+//div that contains the logo, login input, and link to sign up for first time visitors
 import React from 'react';
 import PropTypes from 'prop-types';
 //separate local imports from dependencies
-
-
+import GalleryLogo from './gallery_logo';
+import LoginInput from './login_input';
+import SignUp from './sign_up';
 //declare propTypes prior to component
 
 LoginBlock.propTypes = {
---
+++ js/components/presentational/sign_up_input.js
@@ -0,0 +1,1 @@
+// for new users to sign up with email and password
