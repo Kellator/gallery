7b0116752341025ecb7de64736221d05fe152981
Kellator <k.macleod21@gmail.com>
2017-06-13 23:00:03 -0400

    reducer fixed. store now defined

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,38 +1,45 @@
-import * as actions from './actions/index';
+import * as actions from '../actions/index';
+//import { initialState } from '../store';
+import { combineReducers } from 'redux';
 
-const combineReducers = require('redux').combineReducers;
-const reducer = combineReducers({
-    validation: validationReducer,
-    gallery: galleryReducer,
-    menu: menuReducer
-});
+export const initialState = {
+    authorized: false
+};
 
-const validationReducer = ( state, action ) => {
+const validation = ( state = initialState, action ) => {
     switch(action.type) {
-        case 'CHECK_USER': {  //query server to check if user exists
+        case 'CHECK_USER':   //query server to check if user exists
             return Object.assign({}, state, {
                 email: email,
                 password: password  
             });
-        }
-        case 'DIRECT_TO_SIGNUP': {
+        
+        case 'DIRECT_TO_SIGNUP': 
             return Object.assign({}, state, {
                 authorized: false
             });
-        }
-        case 'ENTER_GALLERY': {  //would be dispatched upon successful server query return
+        
+        case 'ENTER_GALLERY':   //would be dispatched upon successful server query return
             return Object.assign({}, state, {
                 authorized: true
             });
-        }
+        
         default: return state;
     }
 };
 
-const galleryReducer = ( state, action ) => {
-
+const gallery = ( state = 0, action ) => {
+    return state;
 };
 
-const menuReducer = ( state, action ) => {
-
+const menu = ( state = 0, action ) => {
+    return state;
 };
+
+
+
+export default combineReducers({
+    validation: validation,
+    gallery: gallery,
+    menu: menu
+});
--- js/store.js
+++ js/store.js
@@ -1,26 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import createBrowserHistory from 'history/createBrowserHistory';
-import logger from 'redux-logger';
+import logger from 'redux-logger'
 import thunk from 'redux-thunk';
 import reducer from './reducers/index';
 
 const middleware = [
-    logger(),
+    logger,
     thunk
 ];
 
 const enhancers = compose(
     applyMiddleware(...middleware),
-    window.devToolsExtension ? window.devToolsExtension() : f => f  
+    window.devToolsExtension ? window.devToolsExtension() : f => f
 );
-
-export const intialState = {
-    authorized: false
-};
 
 let store = createStore(
     reducer,
-    initialState,
     enhancers
 )
 
