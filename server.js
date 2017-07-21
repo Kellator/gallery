402a9ea13fb03cc1b1a49f8b321091e37841bb98
Kellator <k.macleod21@gmail.com>
2017-07-20 21:43:17 -0400

    Corrected typo in file import and added runServer to coordinate connection to database and the running on the HTTP server.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import config from './config';
 import Exhibit from './models/exhibit';
 import Gallery from './models/gallery';
 import Message from './models/message';
-import UserGallery from './models/user-gallery);
+import UserGallery from './models/user-gallery';
 import UserWall from 'models/user-wall';
 import User from 'models/user';
 import Wall from 'models/wall';
@@ -24,6 +24,30 @@ app.use(express.static('public'));
 
 const server = http.Server(app);
 
+//coordinates the connection to the database, and the running on the HTTP server
+const runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:', + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+//makes file both executable script and a module
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+exports.app = app;
+exports.runServer = runServer;
 
 app.listen((process.env.PORT || 8080), function() {
     console.log('server listening on port 8080');
