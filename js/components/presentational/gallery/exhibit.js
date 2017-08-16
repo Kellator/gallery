3454b56b2154c945febc9d0452a9f7b589d7b780
Kellator <k.macleod21@gmail.com>
2017-08-16 12:56:13 -0400

    Images returned in Exhibits now act as links to source site url.

--- js/components/presentational/gallery/exhibit.js
+++ js/components/presentational/gallery/exhibit.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 //separate local imports from dependencies
 import Menu from '../navigation/action_menu.js';
+// import '../css/index.css';
 
 //declare propTypes prior to component
 
@@ -23,8 +24,8 @@ export default class Exhibit extends React.Component {
         console.log(postedBy);
         return (
             <div>
-                <img className="exhibit-image" alt="image from exhibit" src={exhibitImage}/>
-                <p className="exhibit-title">{title}</p>
+                <a href={siteLink} target="_blank">{<img className="exhibit-image" alt="image from exhibit" src={exhibitImage}/>}</a>
+                <h2 className="exhibit-title"><a href={siteLink} target="_blank">{title}</a></h2>
                 <p className="exhibit-poster">Posted by: {postedBy} </p>
                 <Menu className={"gallery_menu"}/>
             </div>
