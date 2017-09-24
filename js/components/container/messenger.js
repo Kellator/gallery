1c2934933051aff6e5dcf91af981ca3ea0aa0fd3
Kellator <k.macleod21@gmail.com>
2017-09-24 11:06:07 -0400

    Removing socket components and attempting socket-less messenger

--- js/components/container/messenger.js
+++ js/components/container/messenger.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { dispatch } from 'react-redux';
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 
 //separate local imports from dependencies
 import * as actions from '../../actions/index';
@@ -15,22 +15,12 @@ import MessageInput from '../presentational/messenger/message_input';
 // Messenger.propTypes = {
 
 // }
-// MAIN CHAT LOGIC HERE - COMMUNICATE WITH CHAT SERVER (SEND AND RECEIVE MSGS)  THEN PASS DATA RECEIVED FROM 
-// SERVER TO OTHER COMPONENTS TO DISPLAY
+//welcome to {channel}
+// user has joined {channel}
+// last {message} saved to {channel}
 class Messenger extends React.Component {
-// default channel will always load main.  User must choose to enter other channel
-// What does socket need to do:  broadcast :
+
     render() {
-        console.log(this.props);
-        const socket = io();
-        var username = this.props.user.username;
-        socket.emit('chat mounted', username);
-        // what chat has opened  - ?  does this need a join?
-        // socket.emit('welcome', 'Welcome, {user}, to the {channel} chat.')
-        // who has entered the chat
-        // socket.broadcast.emit('broadcast', '{user} has entered the chat.')
-        // load last message saved to DB 
-        // socket.emit('last message', {message})
         console.log(this.props);
         return (
             <div>
